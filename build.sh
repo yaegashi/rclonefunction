42c70e2c3055bbafd62720c92d3214459c2bd83c
YAEGASHI Takeshi <yaegashi@gmail.com>
2021-09-24 15:58:55 +0000

    Add GitHub Actions workflow

--- build.sh
+++ build.sh
@@ -3,4 +3,13 @@
 set -ex
 
 go build -v handler.go
-GOBIN=$PWD go install -v github.com/rclone/rclone@latest
+
+RCLONE_RELEASE_URL=https://api.github.com/repos/rclone/rclone/releases/latest
+RCLONE_DOWNLOAD_URL=$(curl -s $RCLONE_RELEASE_URL | sed -r -n 's,.*"(https://.*linux-amd64.zip)".*,\1,p')
+curl -sL $RCLONE_DOWNLOAD_URL -o rclone.zip
+unzip rclone.zip -d rclone.dir
+mv rclone.dir/rclone-*/rclone .
+rm -rf rclone.zip rclone.dir
+
+rm -f rclonefunction.zip
+zip -r rclonefunction.zip host.json handler rclone QueueTrigger1 TimerTrigger1
